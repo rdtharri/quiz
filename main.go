909ce319c7b23e24bfac22ea364f279ea53b0e4d
Ricky Tharrington <Ricky.Tharrington@sas.com>
2019-05-29 13:18:38 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+)
+func main() {
+    csvFilename := flag.String("csv", "problems.csv", "A csv file in the format of 'question,answer'")
+    flag.Parse()
+
+    file, err := os.Open(*csvFilename)
+    if err != nil {
+        fmt.Printf("Failed to open the CSV file: %s\n", *csvFilename)
+        os.Exit(1)
+    }
+    _ = file
+}
